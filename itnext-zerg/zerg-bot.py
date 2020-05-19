cb0c3b0029243e8ec2fb134bffdc98e5c27cd28c
pgiardiniere <pgiardiniere@gmail.com>
2020-05-18 23:55:53 -0400

    bot selects drones, makes spawning pools

--- itnext-zerg/zerg-bot.py
+++ itnext-zerg/zerg-bot.py
@@ -2,11 +2,45 @@ from pysc2.agents import base_agent
 from pysc2.env import sc2_env
 from pysc2.lib import actions, features
 from absl import app
+from pysc2.lib import actions, features, units
+import random
 
 
 class ZergAgent(base_agent.BaseAgent):
+    def unit_type_is_selected(self, obs, unit_type):
+        if (len(obs.observation.single_select) > 0 and
+                obs.observation.single_select[0].unit_type == unit_type):
+            return True
+
+        if (len(obs.observation.multi_select) > 0 and
+                obs.observation.multi_select[0].unit_type == unit_type):
+            return True
+
+        return False
+
+    def get_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.feature_units
+                if unit.unit_type == unit_type]
+
     def step(self, obs):
         super(ZergAgent, self).step(obs)
+
+        if self.unit_type_is_selected(obs, units.Zerg.Drone):
+            if (actions.FUNCTIONS.Build_SpawningPool_screen.id in
+                    obs.observation.available_actions):
+                x = random.randint(0, 83)
+                y = random.randint(0, 83)
+
+                return actions.FUNCTIONS.Build_SpawningPool_screen('now', (x, y)) # noqa
+
+        drones = [unit for unit in obs.observation.feature_units
+                  if unit.unit_type == units.Zerg.Drone]
+
+        if len(drones) > 0:
+            drone = random.choice(drones)
+
+            return actions.FUNCTIONS.select_point("select_all_type",
+                                                  (drone.x, drone.y))
 
         return actions.FUNCTIONS.no_op()
 
@@ -21,7 +55,8 @@ def main(unused_argv):
                          sc2_env.Bot(sc2_env.Race.random,
                                      sc2_env.Difficulty.very_easy)],
                 agent_interface_format=features.AgentInterfaceFormat(
-                    feature_dimensions=features.Dimensions(screen=84, minimap=64)),  # noqa: E501
+                    feature_dimensions=features.Dimensions(screen=84, minimap=64), # noqa
+                    use_feature_units=True),
                 step_mul=16,
                 game_steps_per_episode=0,
                 visualize=True) as env:
